4524ca8e98000c9ec65b7ae1ba011ded47701ea8
Hidenori Miyamoto <miyamotoh@nttdocomo.com>
2017-07-21 18:24:10 +0900

    add data binding

--- public/app.js
+++ public/app.js
@@ -7,9 +7,11 @@ var learnjs = {};
 //   return $('<div class="problem-view">').text(title);
 // }
 
-learnjs.problemView = function (problemNumber) {
+learnjs.problemView = function (data) {
+  var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
-  view.find('.title').text('Problem #' + problemNumber + ' Coming soon!');
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
   return view;
 }
 
@@ -30,4 +32,21 @@ learnjs.appOnReady = function () {
     learnjs.showView(window.location.hash);
   }
   learnjs.showView(window.location.hash);
+};
+
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Moth",
+    code: "function problem() { return 42 === 6 * __ }"
+  }
+];
+
+learnjs.applyObject = function (obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
 };
